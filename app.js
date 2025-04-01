bd379bec7482df604b9f7925e0effc6bde449cdb
Adam Trzpis <worms3d4@tlen.pl>
2025-04-02 01:05:17 +0200

    Rendering index.ejs
    + Moved database logic into separate file
    + Changed index.html to index.ejs
    + Removed empty file
    + Moved style.css to static directory

--- app.js
+++ app.js
@@ -1,42 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+const app = express();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://kklimasbis:VlDb7cpto2QCZK5V@gooddoctordb.ohib8.mongodb.net/?appName=goodDoctorDB";
+app.use(express.json());
+app.use(express.static('./static'))
+app.set("view engine", "ejs");
+app.use(express.json());
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+/* All routes 
+ * TODO: Think about placing the code for all endpoints in a separate module(s)
+ *
+ */
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index');
 })
 
 app.post('/register', (req, res) => {
-  req.
-  res.send('Add Doctor!')
+  res.send('Add Doctor!');
 })
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Serving GoodDoctor on port:${port}`);
 })
